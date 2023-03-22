8fd3522777a484a3fc26bddb931d935d5d7850fd
Rei-Yi Tan <tanreiyi@gmail.com>
2023-03-21 22:46:39 -0500

    implement delete playlist and updated add playlist

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,37 +1,20 @@
 import React from "react"; 
-import { useState } from "react"; 
 
-const PlaylistNameForm = ({setPlaylistName, setDeleteButtonStyle, setPlaylistButtonStyle}) => {
-    const [formStyle, setFormStyle] = useState("playlist-name-form"); 
-    const hideForm = () => {
-        setFormStyle("hidden");
+/**
+ * Component for creating a playlist in the Sidebar. 
+ * 
+ * @param playlistId - 
+ */
+const Playlist = ({playlistName, playlists, setPlaylists}) => {
+    const handleDelete = () => {
+        const newList = playlists.filter((playlist) => playlist.playlistName !== playlistName);
+        setPlaylists(newList);
     }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setPlaylistName(e.target.playlistName.value);
-        setPlaylistButtonStyle("sidebar-playlist-button");
-        setDeleteButtonStyle("sidebar-delete-button");
-        hideForm();
-    }
-
-    return (
-        <form onSubmit={handleSubmit}> 
-            <input type="text" className={formStyle} placeholder="Enter playlist name:" name="playlistName" />
-        </form>
-    );
-}
-
-const Playlist = () => {
-    const [playlistName, setPlaylistName] = useState("");
-    const [playlistButtonStyle, setPlaylistButtonStyle] = useState("hidden");
-    const [deleteButtonStyle, setDeleteButtonStyle] = useState("hidden");
 
     return (
         <div>
-            <PlaylistNameForm setPlaylistName={setPlaylistName} setPlaylistButtonStyle={setPlaylistButtonStyle}
-                setDeleteButtonStyle={setDeleteButtonStyle} />
-            <button className={playlistButtonStyle}>{playlistName}</button>
-            <button className={deleteButtonStyle}>-</button>
+            <button className="sidebar-playlist-button">{playlistName}</button>
+            <button className="sidebar-delete-button" onClick={handleDelete}>-</button>
         </div>
     );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,20 +3,89 @@ import { useState } from "react";
 import Searchbar from "./Searchbar";
 import Playlist from "./Playlist"; 
 
+/**
+ * Component for naming a playlist. This component is by default hidden, and 
+ * is displayed when a user clicks on the add playlist button. 
+ * 
+ * @param playlistNameFormStyle - The css className for the input form. This state is passed from Sidebar. 
+ * @param onFormSubmit - A function passed from Sidebar used to pass user input back to the parent component.
+ * 
+ * @returns The playlist name form component.
+ */
+const PlaylistNameForm = ({ playlistNameFormStyle, onFormSubmit }) => {
+    const [value, setValue] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setValue("");
+        onFormSubmit(e.target.playlistName.value);
+    }
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                className={playlistNameFormStyle}
+                name="playlistName" 
+                value={value} 
+                placeholder="Enter a playlist name:"
+                onChange={handleChange} />
+        </form>
+    );
+}
+
+/**
+ * Component for the sidebar containing the Searchbar, add playlist button, and playlists.
+ * 
+ * @returns The Sidebar component.
+ */
 const Sidebar = () => {
     const [playlists, setPlaylists] = useState([]); 
-    const [id, setIdNum] = useState(0); 
-    const addPlaylist = () => {
-        setPlaylists(playlists.concat(<Playlist key={id}/>)); 
-        setIdNum(id + 1); 
+    const [addPlaylistButtonStyle, setAddPlaylistButtonStyle] = useState("add-playlist-button");
+    const [playlistNameFormStyle, setPlaylistNameFormStyle] = useState("hidden"); 
+    const showNameForm = () => {
+        setAddPlaylistButtonStyle("hidden");
+        setPlaylistNameFormStyle("playlist-name-form");
+    }
+
+    /**
+     * Adds a new empty playlist to this component's list of playlists. 
+     * Unhides the add play list button and hides the playlist name form. 
+     * 
+     * @param playlistName - The name of the new playlist.
+     */
+    const handleSubmit = (playlistName) => {
+        if (playlists.find((playlist) => playlist.playlistName === playlistName) !== undefined) {
+            //add code saying you cannot have duplicate names
+            console.log("no duplicates allowed");
+        }
+        else { 
+            const newPlaylist = {
+            playlistName: playlistName,
+            songs: []
+            };
+            setPlaylists([...playlists, newPlaylist]);
+        }
+        setAddPlaylistButtonStyle("add-playlist-button");
+        setPlaylistNameFormStyle("hidden");
     }
 
     return (
         <div id="sidebar">
             <Searchbar />
-            <button onClick={addPlaylist} id="add-playlist-button">+ Add a playlist</button>
+            <PlaylistNameForm playlistNameFormStyle={playlistNameFormStyle} onFormSubmit={handleSubmit} />
+            <button onClick={showNameForm} className={addPlaylistButtonStyle}>+ Add a playlist</button>
             <div id="sidebar-playlists">
-                {playlists}
+                {playlists.slice().reverse().map((playlist) => (
+                    <Playlist 
+                        key={playlist.playlistName} 
+                        playlistName={playlist.playlistName}
+                        playlists={playlists}
+                        setPlaylists={setPlaylists}
+                    />
+                ))}
             </div>
         </div>
     );
