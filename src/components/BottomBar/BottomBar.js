b1f9a99db0ce4d6b27226859a5e6684992ff9b45
Rei-Yi Tan <tanreiyi@gmail.com>
2023-03-27 00:33:59 -0500

    give pause and play button a shared pp-button class to simplify css

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "./style.css";
+import { useState } from "react";
 
 /**
  * Component for the bottom bar of the webpage.
  * 
- * @param currentlyPlaying - Information for the song that is currently playing. Part of index.js state.
- * @param setCurrentlyPlaying - Used to update state of currentlyPlaying. 
+ * @param currentSong - Information for the song that is currently playing. Part of index.js state.
+ * @param setCurrentSong - Used to update state of currentSong.
  */
-const BottomBar = ({currentlyPlaying, setCurrentlyPlaying}) => {
+const BottomBar = ({currentSong, setCurrentSong}) => {
+    const [buttonState, setButtonState] = useState("play-button pp-button");
+    const handlePlay = () => {
+        setButtonState("pause-button pp-button");
+    }
+    const handlePause = () => {
+        setButtonState("play-button pp-button");
+    }
     return (
         <div id="bottom-bar">
-            {currentlyPlaying}
+            <span className="bottom-bar-title">{currentSong.title}</span>
+            <span className="bottom-bar-artist">{currentSong.artist}</span>
+            <button className={buttonState} onClick={(buttonState==="play-button pp-button") ? handlePlay : handlePause}></button>
         </div>
     )
 }
