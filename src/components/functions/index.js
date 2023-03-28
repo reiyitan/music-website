8f6ce746e4cd9b4b261eee53cb3b743055d1964d
Rei-Yi Tan <tanreiyi@gmail.com>
2023-03-28 02:44:37 -0500

    placeholder function for loadAllSongs()

--- src/components/functions/index.js
+++ src/components/functions/index.js
@@ -1,2 +1,3 @@
 export { default as loadPlaylistSongs } from "./loadPlaylistSongs";
 export { default as loadPlaylists } from "./loadPlaylists";
+export { default as loadAllSongs } from "./loadAllSongs";
---
+++ src/components/functions/loadAllSongs.js
@@ -0,0 +1,12 @@
+export default function loadAllSongs() {
+    return (
+        [
+            {"title": "song 1", "artist": "artist 1", "album": "some album", "length": "1:23"},
+            {"title": "another song", "artist": "another artist", "album": "another album", "length": "2:34"},
+            {"title": "song 1", "artist": "different artist", "album": "different album", "length": "5:15"},
+            {"title": "song 2", "artist": "artist 1", "album": "some album", "length": "2:54"},
+            {"title": "song 3", "artist": "artist 1", "album": "some album", "length": "3:15"},
+            {"title": "abcdefg", "artist": "3rd artist", "album": "3rd album", "length": "4:31"},
+        ]
+    );
+}
