696d851c820c0afd2af86f0b24702880681eaf2d
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-11 03:32:58 -0500

    update placeholder songs

--- src/components/functions/createPlayback.js
+++ src/components/functions/createPlayback.js
@@ -1,7 +1,13 @@
 import { Howl } from "howler"; 
 
 export default function createPlayback(title, artist, album, length) {
-    const path = `../../songs/${title}.mp3`;
+    let path;
+    if (artist !== "Kevin Macleod") {
+        path = `../../songs/Flight of the Bumblebee.mp3`;
+    }
+    else {
+        path = `../../songs/${title}.mp3`;
+    }
     const playback = new Howl({
         src: [path],
         volume: 0.05,
