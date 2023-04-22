39e53bfdf5cb903c344f2c620cd3d3b0c0069168
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-22 03:30:15 -0500

    modify formatTime

--- src/components/Seekbar/Seekbar.js
+++ src/components/Seekbar/Seekbar.js
@@ -46,9 +46,9 @@ const Seekbar = () => {
     }, [playbackRef]);
 
     const formatTime = (duration) => {
+        if (duration < 0) return "0:00";
         const minutes = Math.floor(duration / 60);
         const seconds = Math.floor(duration % 60);
-        if (minutes === -1) return "0:00";
         return `${minutes}:${seconds.toString().padStart(2, '0')}`;
     }
 
